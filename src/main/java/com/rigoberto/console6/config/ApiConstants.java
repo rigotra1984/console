204d2a5df48b9185b1df468f7b8f9b9278fe1bf1
Rigoberto Pérez Rajadel <rigotra1984@gmail.com>
2023-07-14 16:27:52 -0400

    actualizacion a liquibase y mapper
    se quita flyway y se agrega liquibase
    el maper se crea con la inyeccion de dependencia

--- src/main/java/com/rigoberto/console6/config/ApiConstants.java
+++
@@ -1,5 +0,0 @@
-package com.rigoberto.console6.config;
-
-public class ApiConstants {
-    public static final int PAGE_SIZE = 20;
-}
--- src/main/resources/db/migration/V1__schema.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE event
-(
-    id serial not null primary key,
-    date timestamp not null,
-    priority       VARCHAR(255),
-    description   VARCHAR(255)
-);
